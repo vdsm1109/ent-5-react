8b04f507f3d6b6a17eca2ac079c62a5497103758
Victor <vdsm1109@gmail.com>
2024-05-04 18:58:19 -0400

    tarea

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-dom/client']
+    }
   }
 })
